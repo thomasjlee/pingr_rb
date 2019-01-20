5c7d8d6feb4c590e1227622d75095db0489bba8a
Thomas Lee <leethomasjae@gmail.com>
2019-01-20 17:50:00 -0600

    Comment out code for presence feature

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,16 +6,4 @@ class User < ApplicationRecord
   def unread_pings
     self.pings.where(read_at: nil).order(created_at: :desc)
   end
-
-  def appear
-    ActionCable.server.broadcast('appearance_channel', user_id: id, online: true)
-  end
-
-  def disappear
-    ActionCable.server.broadcast('appearance_channel', user_id: id, online: false)
-  end
-
-  def online?
-    Redis.new(url: ENV.fetch('REDISTOGO_URL', 'redis://localhost:6379')).get("user_#{self.id}_online") == "1"
-  end
 end
