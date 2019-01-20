3bb32a14b09bb24b9c95896fd985a3deb3baad64
Thomas Lee <leethomasjae@gmail.com>
2019-01-20 12:02:47 -0600

    Fix duplicate notifications bug, add pinged animation

--- app/assets/javascripts/channels/pings.js
+++ app/assets/javascripts/channels/pings.js
@@ -1,27 +1,61 @@
-App.pings = App.cable.subscriptions.create("PingsChannel", {
-  connected: function() {
-    // Called when the subscription is ready for use on the server
-  },
+document.addEventListener('turbolinks:load', function() {
+  if (shouldConnect()) {
+    if (!App.pings) {
+      App.pings = App.cable.subscriptions.create("PingsChannel", {
+        connected: function() {
+          // Called when the subscription is ready for use on the server
+        },
 
-  disconnected: function() {
-    // Called when the subscription has been terminated by the server
-  },
+        disconnected: function() {
+          // Called when the subscription has been terminated by the server
+        },
 
-  // TESTME
-  received: function(data) {
-    console.dir(data)
-    var unreadPings = document.querySelector('[data-unread_pings]')
-    if (unreadPings) {
-      unreadPings.insertAdjacentHTML('afterbegin', data.html['ping'])
-    }
+        // TESTME
+        received: function(data) {
+          var unreadPingsUl     = this.unreadPingsUl()
+          var markAllAsReadForm = this.markAllAsReadForm()
 
-    unreadPingsCount = document.querySelector('[data-unread_pings_count]')
-    count = parseInt(unreadPingsCount.textContent)
-    unreadPingsCount.textContent = ++count
+          if (unreadPingsUl) {
+            unreadPingsUl.insertAdjacentHTML('afterbegin', data.html['ping'])
 
-    var markAllAsRead = document.querySelector('form[action$="mark_all_as_read"]')
-    if (!markAllAsRead) {
-      unreadPings.insertAdjacentHTML('afterend', data.html['mark_all_as_read'])
+            if (!markAllAsReadForm) {
+              unreadPingsUl.insertAdjacentHTML('afterend', data.html['mark_all_as_read'])
+            }
+          }
+
+          this.updateUnreadPingsCount()
+          this.exclaim()
+        },
+
+        updateUnreadPingsCount: function() {
+          unreadPingsCount = this.unreadPingsCountSpan()
+          count = parseInt(unreadPingsCount.textContent)
+          unreadPingsCount.textContent = ++count
+          unreadPingsCount.classList.add('badge-danger')
+          unreadPingsCount.classList.remove('badge-secondary')
+        },
+
+        exclaim: function() {
+          this.unreadPingsCountSpan().insertAdjacentHTML('beforebegin', '<span class="exclaim">!</span>')
+        },
+
+        unreadPingsUl: function() {
+          return document.querySelector('[data-unread_pings]')
+        },
+
+        markAllAsReadForm: function() {
+          return document.querySelector('form[action$="mark_all_as_read"]')
+        },
+
+        unreadPingsCountSpan: function() {
+          return document.querySelector('[data-unread_pings_count]')
+        }
+      })
     }
   }
-});
+
+  function shouldConnect() {
+    // User is authenticated
+    return Boolean(document.querySelector('meta[name="current_user"]'))
+  }
+})
