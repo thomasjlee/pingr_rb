7debbea7b656c9eaaa22c051851cda89a2c937dc
Thomas Lee <leethomasjae@gmail.com>
2019-01-21 15:25:57 -0600

    Replace user emails with a username

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,10 @@ class User < ApplicationRecord
   include Clearance::User
   has_many :pings, foreign_key: :recipient_id
 
+  def username
+    email.split('@').first
+  end
+
   # TESTME
   def unread_pings
     self.pings.where(read_at: nil).order(created_at: :desc)
