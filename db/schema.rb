54979aed1cfd1f5c72abc5b3dd499265490c2f58
Thomas Lee <leethomasjae@gmail.com>
2019-01-21 15:27:49 -0600

    Remove username from users table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_21_142244) do
+ActiveRecord::Schema.define(version: 2019_01_21_212619) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,7 +30,6 @@ ActiveRecord::Schema.define(version: 2019_01_21_142244) do
     t.string "confirmation_token", limit: 128
     t.string "remember_token", limit: 128, null: false
     t.integer "online", default: 0
-    t.string "username"
     t.index ["email"], name: "index_users_on_email"
     t.index ["remember_token"], name: "index_users_on_remember_token"
   end
