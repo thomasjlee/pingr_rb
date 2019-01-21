47ede5d20ef792c98f91a2e2ef101b5ab5c3e6ab
Thomas Lee <leethomasjae@gmail.com>
2019-01-21 15:40:55 -0600

    Add order to users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def index
     if signed_in?
-      @users = User.where.not(id: current_user.id)
+      @users = User.where.not(id: current_user.id).order(created_at: :desc)
     else
-      @users = User.all
+      @users = User.all.order(created_at: :desc)
     end
   end
 end
